f11b31a9cda2e905df3d1722cb0cbca226c080ae
Maciej Wasilewski <wasilewski.maciej20@gmail.com>
2021-09-29 18:14:10 +0200

    Update tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -4,6 +4,7 @@ import unittest
 import os
 
 from lit_script import Interpreter
+import lit_script.expressions as ex
 
 TESTS_LOCATION = "language/tests"
 
@@ -20,6 +21,65 @@ class TestLitScript(unittest.TestCase):
             with open(test_file_path, "r") as f:
                 code = f.read()
                 interpreter.read(code)
+
+class TestExpressions(unittest.TestCase):
+    
+    def test_create_function(self):
+
+        def increase(a: ex.Atom):
+            x = a.value
+            return ex.Atom(
+                value=a.value + 100,
+                type="Int"
+            )
+
+        increase_fn = ex.Function(
+            argument=ex.Variable(name="x"),
+            expression=ex.Expression(),
+            override=increase
+        )
+
+        self.assertEqual(
+            increase_fn(ex.Atom(value=100, type="Int")),
+            ex.Atom(value=200, type="Int")
+        )
+
+        ex.Expression.context.add_variable(
+            ex.Variable(name="increase"),
+            ex.AtomExpression(atom=increase_fn)
+        )
+
+    def test_call_function(self):
+        def increase(a: ex.Atom):
+            return ex.Atom(
+                value=a.value + 100,
+                type="Int"
+            )
+
+        increase_fn = ex.Function(
+            argument=ex.Variable(name="x"),
+            expression=ex.Expression(),
+            override=increase
+        )
+
+        ex.Expression.context.add_variable(
+            ex.Variable(name="increase"),
+            ex.AtomExpression(atom=increase_fn)
+        )
+        
+        
+        e = ex.FunctionCallExpression(
+            fncall=ex.FunctionCall(
+                fun=ex.VariableExpression(ex.Variable(name="increase")),
+                arg=ex.AtomExpression(ex.Atom(value=100, type="Int"))
+            )
+        )
+
+        result = e.resolve()
+
+        self.assertIsInstance(result, ex.Atom)
+        self.assertEqual(result.value, 200)
+
 
 if __name__ == '__main__':
     unittest.main()
