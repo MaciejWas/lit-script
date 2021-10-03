823f242fecb6f2838ad0240d88afabdf281eb355
Maciej Wasilewski <wasilewski.maciej20@gmail.com>
2021-10-03 14:51:51 +0200

    bugfix

---
+++ src/compiler.py
@@ -0,0 +1,16 @@
+from .expressions import Line, Context
+
+Compilable = list[Line]
+
+
+def create_compiler() -> "Compiler":
+    context = Context()
+    return Compiler(context)
+
+
+class Compiler:
+    def __init__(self, context: Context):
+        self.context = context
+
+    def compile(self, lines: Compilable):
+        print("Compiled! :)")
