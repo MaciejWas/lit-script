d6aa34ec3b4758a628be3cbc128e53afce78fd9d
Maciej Wasilewski <wasilewski.maciej20@gmail.com>
2021-10-03 14:52:26 +0200

    add compiler to read

--- src/interpreter.py
+++ src/interpreter.py
@@ -2,6 +2,7 @@ from lark import Lark
 from lark.visitors import TransformerChain
 
 from .transformer import create_transformer
+from .compiler import Compilable, Compiler, create_compiler
 
 
 class CompileLog:
@@ -21,19 +22,19 @@ class CompileLog:
 class Interpreter:
     def __init__(self):
         self.parser: Lark = self.load_parser()
-        self.transformer: TransformerChain = self.load_transformer()
+        self.transformer: TransformerChain = create_transformer()
+        self.compiler: Compiler = create_compiler()
 
     def load_parser(self) -> Lark:
         with open("language/grammar.lark", "r") as f:
             grammar = f.read()
         return Lark(grammar, start="all")
 
-    def load_transformer(self) -> TransformerChain:
-        return create_transformer()
-
     def read(self, line: str):
         tree = self.parser.parse(line)
         print(tree.pretty())
 
-        transformed_tree = self.transformer.transform(tree)
-        print(transformed_tree)
+        program: Compilable = self.transformer.transform(tree)
+        print(program)
+
+        self.compiler.compile(program)
